f8705aacd850cf01cd33da8422460db52bc25690
BrunoAssis <brunoassis@gmail.com>
2011-10-23 13:22:53 -0200

    First commit from Mac

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,21 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+    @counter = session[:counter]
   end
 
+  private
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
 end

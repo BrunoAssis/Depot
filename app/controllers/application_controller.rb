8d2347a85c60999f7c56b4ea2f137c758986c169
Bruno Assis <brunoassis@gmail.com>
2012-02-02 18:35:45 -0200

    Added login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authorize
   before_filter :prepare_time_for_display
   def prepare_time_for_display
     @current_time = Time.now
@@ -12,5 +13,12 @@ class ApplicationController < ActionController::Base
     cart = Cart.create
     session[:cart_id] = cart.id
     cart
+  end
+
+  protected
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
   end
 end
